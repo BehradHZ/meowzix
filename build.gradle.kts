385a17b97618f07f71dde5250a4da0ff71a515d4
Behrad Hozouri <bhvsrt2006@gmail.com>
2026-09-17 17:54:13 +0330

    build: modernize AGP 9 Kotlin processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.3.21" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.devtools.ksp") version "2.3.12" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
 }
