979092029701033b0bbafc61001813a2e0a83ae7
BehradHZ <bhvsrt@gmail.com>
2026-09-17 19:29:18 +0200

    build: add reproducible Gradle wrapper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,44 @@
 pluginManagement {
     repositories {
-        google()
+        maven("https://redirector.gvt1.com/edgedl/android/maven2") {
+            name = "GoogleMavenRedirector"
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*)?")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*)?")
+            }
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
+        maven("https://redirector.gvt1.com/edgedl/android/maven2") {
+            name = "GoogleMavenRedirector"
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*)?")
+                includeGroupByRegex("com\\.google\\.android(\\..*)?")
+                includeGroupByRegex("com\\.google\\.dagger(\\..*)?")
+            }
+        }
+        google {
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*)?")
+                includeGroupByRegex("com\\.google\\.android(\\..*)?")
+                includeGroupByRegex("com\\.google\\.dagger(\\..*)?")
+            }
+        }
     }
 }
 
