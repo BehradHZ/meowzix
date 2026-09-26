dfae6c69af22c81a8ce237887938e17847ef7637
Behrad Hozouri <bhvsrt2006@gmail.com>
2026-09-26 12:24:04 +0330

    Resume Telegram send queue on app startup

--- app/src/main/java/dev/behradhz/meowzix/MeowzixApplication.kt
+++ app/src/main/java/dev/behradhz/meowzix/MeowzixApplication.kt
@@ -2,6 +2,15 @@ package dev.behradhz.meowzix
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dev.behradhz.meowzix.domain.telegram.TelegramForwardRepository
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MeowzixApplication : Application()
+class MeowzixApplication : Application() {
+    @Inject lateinit var telegramForwardRepository: TelegramForwardRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        telegramForwardRepository.initialize()
+    }
+}
